68bda4b99ea6e8a44d79ce7a390412c7e649ec84
aliciaortiza <aliciaortiza@outlook.com>
2025-09-09 16:20:59 -0600

    semantic

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,3 @@
-// Mostrar el año actual
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-// Mostrar la fecha de última modificación
 document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
