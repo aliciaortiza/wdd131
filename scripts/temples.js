85fb32344071aadd6c66ac6662cc4f91ebf549e0
aliciaortiza <aliciaortiza@outlook.com>
2025-09-16 12:35:43 -0600

    finish

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,6 +2,16 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
 
-document.getElementById("menu").addEventListener("click", () => {
-    document.querySelector("nav ul").classList.toggle("open");
+const menuButton = document.getElementById("menu");
+const navList = document.querySelector("nav ul");
+
+menuButton.addEventListener("click", () => {
+    navList.classList.toggle("open");
+
+    // Cambia el ícono del botón entre ≡ y ✖
+    if (navList.classList.contains("open")) {
+        menuButton.textContent = "✖";
+    } else {
+        menuButton.textContent = "≡";
+    }
 });
