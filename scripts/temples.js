6fef739da01c634d95e8d3578bdeb76179a61673
aliciaortiza <aliciaortiza@outlook.com>
2025-09-15 23:02:14 -0600

    changes 15/9

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,7 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+document.getElementById("menu").addEventListener("click", () => {
+    document.querySelector("nav ul").classList.toggle("open");
+});
